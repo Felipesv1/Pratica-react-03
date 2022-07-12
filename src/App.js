56b49f5e9a3288fd5057544fb4f2de198271cd0b
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-07-12 15:54:28 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <p>"Com Grandes poderes , vem Grandes responsabilidades"</p>
+      <p>tio ben.</p>
+    </div>
+  );
+}
